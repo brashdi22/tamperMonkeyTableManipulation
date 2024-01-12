5042b285725098df54a822b6c41435966d1ea20e
Ismail Al Brashdi <brashdi22@hotmail.com>
2024-01-12 00:26:40 +0000

    Fix a bug
    A bug with the show button: if you hide a column, then hide the last
     row, then press the show button, only the row will be shown.

--- main.js
+++ main.js
@@ -77,7 +77,7 @@ function hideColsRows(){
 
         // The id of the respective checkbox: `${table.id}-row${index}`
         // Get the checkbox and uncheck it
-        const checkbox = document.getElementById(`${cell.parentElement.parentElement.parentElement.id}-row${cell.parentElement.rowIndex - 1}`);
+        const checkbox = document.getElementById(`${cell.parentElement.parentElement.parentElement.id}-row${cell.parentElement.rowIndex - 2}`);
         checkbox.checked = false;
     });
 
@@ -105,7 +105,7 @@ function showColsRows(){
 
         // The id of the respective checkbox: `${table.id}-row${index}`
         // Get the checkbox and check it
-        const checkbox = document.getElementById(`${cell.parentElement.parentElement.parentElement.id}-row${cell.parentElement.rowIndex - 1}`);
+        const checkbox = document.getElementById(`${cell.parentElement.parentElement.parentElement.id}-row${cell.parentElement.rowIndex - 2}`);
         checkbox.checked = true;
     });
 
